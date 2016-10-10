63b50ee76f21aa848d5656f46425a9ab9cda090b
alexionita95 <ionita.alexandru95@gmail.com>
2016-10-10 23:53:07 +0300

    Added Hello World Windows Forms in C#

---
+++ HelloWorldForms/HelloWorldForms/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HelloWorldForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.countBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // countBtn
+            // 
+            this.countBtn.Location = new System.Drawing.Point(45, 25);
+            this.countBtn.Name = "countBtn";
+            this.countBtn.Size = new System.Drawing.Size(160, 75);
+            this.countBtn.TabIndex = 0;
+            this.countBtn.Text = "button1";
+            this.countBtn.UseVisualStyleBackColor = true;
+            this.countBtn.Click += new System.EventHandler(this.countBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 134);
+            this.Controls.Add(this.countBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button countBtn;
+    }
+}
+
---
+++ HelloWorldForms/HelloWorldForms/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldForms
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0; //Integer to count the clicks
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void countBtn_Click(object sender, EventArgs e)
+        {
+            counter++; //Increment counter (like C++)
+            countBtn.Text = $"Clicks: {counter}"; //Show the text on the Button (String format)
+        }
+    }
+}
