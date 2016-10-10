5ae3518e1385be61c26276c309db87cbf46f8b49
alexionita95 <ionita.alexandru95@gmail.com>
2016-10-10 23:44:45 +0300

    Hello World Console Application in C#

---
+++ HelloWorldCSharp/HelloWorldCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace HelloWorldCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!"); //Display "Hello World!" in Console
+            ReadKey(); //Wait for a key so you can see the message
+        }
+    }
+}
